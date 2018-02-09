65a90d569afc807f0426d09e466a88c390286081
EnsioS <ensiosuonpera@luukku.com>
2018-02-09 18:49:46 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">          
+          <h1 className="App-title">Duck</h1>
+        </header>
+        <p className="App-intro">
+          To do ..
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App
